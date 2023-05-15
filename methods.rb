2c48e5dc99a2d66a95ab3470e3f7fd72e11604e3
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-05-15 16:53:19 +0300

    Complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+puts add(2,3)
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+   number/2
+end
+puts halve(8) 
